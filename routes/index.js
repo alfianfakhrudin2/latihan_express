2d7b966346bc2c7f08cae2f533da3c5d398166ba
Alfian <alfianfakhrudin11@gmail.com>
2024-08-05 02:17:49 +0700

    Restful API get data users done

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,23 @@ const express = require('express')
 //init express router
 const router = express.Router();
 
+//import verifyToken
+const verifyToken = require('../middlewares/auth');
+
 //import register controller
 const registerController = require('../controllers/RegisterController');
 
 //import login controller
 const loginController = require('../controllers/LoginController');
 
+//import user controller
+const userController = require('../controllers/UserController');
+
 //import validate register and login
 const { validateRegister, validateLogin } = require('../utils/validators/auth');
+
+//define route for user
+router.get('/admin/user', verifyToken, userController.findUsers);
 
 //define route for register
 router.post('/register', validateRegister, registerController.register);
