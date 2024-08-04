203d75f07f8bb33dd4084a63ec18485f2d17445b
Alfian <alfianfakhrudin11@gmail.com>
2024-08-04 18:08:36 +0700

    Restfull api auth done

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,18 @@
+// Import express
 const express = require('express');
 
+// Import jwt
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
+    // Get token
     const token = req.headers['authorization'];
-    if(!token) return res.status(401).json({message: 'Unauthenticated'});
+    if (!token) return res.status(401).json({ message: 'Unauthenticated.' });
 
+    // Verify token
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if(err) return res.status(401).json({message: 'Invalid token'});
-
-        req.userId = jwt.decoded.id;
+        if (err) return res.status(401).json({ message: 'Invalid token' });
+        req.userId = decoded.id;
         next();
     });
 };
